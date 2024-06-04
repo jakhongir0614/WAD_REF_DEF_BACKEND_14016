023208017b9f09826b5da41762ef14dc989eef81
jakhongir0614 <jokhisgreat@gmail.com>
2024-06-04 20:32:45 +0500

    Add project files.

---
+++ WAD_REFDEF_BACKEND_14016/WeatherForecast.cs
@@ -0,0 +1,13 @@
+namespace WAD_REFDEF_BACKEND_14016
+{
+    public class WeatherForecast
+    {
+        public DateOnly Date { get; set; }
+
+        public int TemperatureC { get; set; }
+
+        public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+
+        public string? Summary { get; set; }
+    }
+}
