36f39be4648ebfa329c56eb244ea5ccc85adfcc3
jakhongir0614 <jokhisgreat@gmail.com>
2024-06-04 23:01:10 +0500

    Backend Finished

---
+++ WAD_REFDEF_BACKEND_14016/Data/Migrations/20240604173653_Updated.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace WAD_REFDEF_BACKEND_14016.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class Updated : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
